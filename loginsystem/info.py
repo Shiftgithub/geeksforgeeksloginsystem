00e49533001516d35b8e64677d31857f97f5eb0e
shiftgithub <mhturanmohammad@gmail.com>
2023-02-28 20:53:38 +0600

    push

--- loginsystem/info.py
+++
@@ -1,8 +0,0 @@
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'kabirariyan14@gmail.com'
-EMAIL_HOST_USER = 'turan.sucse@gmail.com'
-EMAIL_HOST_PASSWORD = 'gfececclebvxzmfd'
-# EMAIL_HOST_PASSWORD = 'kctrdsugukkoeagu'
-EMAIL_PORT = 587
